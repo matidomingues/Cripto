a1960053eb0e4cf2a773b6956876cfa722adccc6
mannias <mannias@gmail.com>
2014-05-29 11:09:50 -0300

    fixes in B calculation, now it should word correctly

--- src/main.c
+++ src/main.c
@@ -1,8 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
-typedef int DWORD;
-typedef short WORD;
+typedef unsigned int DWORD;
+typedef unsigned short WORD;
 
 #pragma pack(push, 1)
 
@@ -32,6 +33,8 @@ typedef struct tagBITMAPINFOHEADER
     DWORD biClrImportant;  //number of colors that are important
 }BITMAPINFOHEADER;
 #pragma pack(pop)
+
+
 unsigned char *LoadBitmapFile(char *filename, BITMAPINFOHEADER *bitmapInfoHeader){
 	FILE *filePtr; //our file pointer
 	BITMAPFILEHEADER bitmapFileHeader; //our bitmap file header
@@ -86,18 +89,73 @@ unsigned char *LoadBitmapFile(char *filename, BITMAPINFOHEADER *bitmapInfoHeader
 
 }
 
-int main(void){
-	BITMAPINFOHEADER bitmapInfoHeader;
-	unsigned char *bitmapData;
-	bitmapData = LoadBitmapFile("20x20.bmp",&bitmapInfoHeader);
-	int i = 0, w = 0;
-	for(i=0; i<bitmapInfoHeader.biHeight; i++){
-		for(w=0; w<bitmapInfoHeader.biWidth ; w++){
-			printf("%2x ",*bitmapData);
-			bitmapData++;
-		}
-		printf("\n");
+unsigned char* calculateBits(unsigned char *bitmapData, int k){
+	unsigned char *arr, *aux;
+	int i, l[k];
+	int pot;
+	int num;
+	l[0] = 8;
+	for(i = 1; i < k; i++){
+		printf("%d - %d - %f\n",l[i-1], k-(i-1), ceil(8/(float)3));
+		l[i] = l[i-1] - ceil(l[i-1]/(float)(k-(i-1)));
 	}
+	for(i=0; i<k;i++){
+		printf("%d\n",l[i]);
+	}
+	aux = arr = (unsigned char*)malloc(k);
+
+	for(i = 0; i < k; i++){
+		pot = (int)pow(2,ceil(l[i]/(float)(k-i)));
+		num = 256-pot;
+		*arr = (*bitmapData & num);
+		*arr /= pot;
+		bitmapData++;
+		arr++;
+
+	}
+	return aux;
+}
+
+int calculateB(unsigned char *bitmapData, unsigned char *calculatedA, int k){
+	int i;
+	long sum = 0;
+	for(i=0; i<k; i++){
+		sum += (*bitmapData * *calculatedA);
+		bitmapData++;
+		calculatedA++;
+	}
+	return sum % 251;
+}
+
+void printBMPMatrix(unsigned char *bitmapData, BITMAPINFOHEADER infoHeader){
+	int i,w;
+	for(i=0; i<infoHeader.biHeight; i++){
+			for(w=0; w<infoHeader.biWidth ; w++){
+				printf("%2x ",*bitmapData);
+				bitmapData++;
+			}
+			printf("\n");
+		}
+}
+
+int main(void){
+	BITMAPINFOHEADER infoHeaders[6];
+	unsigned char *bitmapData[6];
+	bitmapData[0] = LoadBitmapFile("20x20.bmp",&infoHeaders[0]);
+	bitmapData[1] = LoadBitmapFile("20x20-1.bmp",&infoHeaders[1]);
+	bitmapData[2] = LoadBitmapFile("20x20-2.bmp",&infoHeaders[2]);
+	bitmapData[3] = LoadBitmapFile("20x20-3.bmp",&infoHeaders[3]);
+	bitmapData[4] = LoadBitmapFile("20x20-4.bmp",&infoHeaders[4]);
+	bitmapData[5] = LoadBitmapFile("20x20-5.bmp",&infoHeaders[5]);
+	unsigned char *data = calculateBits(bitmapData[1], 3);
+	int num1 = calculateB(bitmapData[0], data, 3);
+	data = calculateBits(bitmapData[2], 3);
+	int num2 = calculateB(bitmapData[0], data, 3);
+	data = calculateBits(bitmapData[3], 3);
+	int num3 = calculateB(bitmapData[0], data, 3);
+	printf("%d %d %d\n", num1, num2, num3);
+	printBMPMatrix(bitmapData[0], infoHeaders[0]);
+
 }
 
 
